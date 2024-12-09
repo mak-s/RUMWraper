803cf3984241db53a57420cf269a9dc8fbc79594
Manish <developer.manish915@gmail.com>
2024-12-10 00:18:17 +0530

    Package setup
    setup packages for wrapping bugsnag, datadog, embrace and instabug.

--- Package.swift
+++ Package.swift
@@ -5,19 +5,58 @@ import PackageDescription
 
 let package = Package(
     name: "RumWrapper",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "RumWrapper",
-            targets: ["RumWrapper"]),
+            name: "BugsnagWrapper",
+            targets: ["BugsnagWrapper"]),
+        .library(
+            name: "DatadogWrapper",
+            targets: ["DatadogWrapper"]),
+        .library(
+            name: "EmbraceWrapper",
+            targets: ["EmbraceWrapper"]),
+        .library(
+            name: "InstabugWrapper",
+            targets: ["InstabugWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bugsnag/bugsnag-cocoa", exact: "6.30.2"),
+        .package(url: "https://github.com/DataDog/dd-sdk-ios", exact: "2.20.0"),
+        .package(url: "https://github.com/Instabug/Instabug-SP", exact: "14.0.0"),
+        .package(url: "https://github.com/embrace-io/embrace-apple-sdk", exact: "6.5.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RumWrapper"),
-        .testTarget(
-            name: "RumWrapperTests",
-            dependencies: ["RumWrapper"]),
+            name: "BugsnagWrapper",
+            dependencies: [
+                .product(name: "Bugsnag", package: "bugsnag-cocoa")
+            ]
+        ),
+        .target(
+            name: "DatadogWrapper",
+            dependencies: [
+                .product(name: "DatadogCore", package: "dd-sdk-ios"),
+                .product(name: "DatadogRUM", package: "dd-sdk-ios")
+            ]
+        ),
+        .target(
+            name: "EmbraceWrapper",
+            dependencies: [
+                .product(name: "EmbraceCore", package: "embrace-apple-sdk"),
+            ]
+        ),
+        .target(
+            name: "InstabugWrapper",
+            dependencies: [
+                .product(name: "Instabug", package: "Instabug-SP"),
+            ]
+        ),
+        .testTarget(name: "BugsnagWrapperTests", dependencies: ["BugsnagWrapper"]),
+        .testTarget(name: "DatadogWrapperTests", dependencies: ["DatadogWrapper"]),
+        .testTarget(name: "EmbraceWrapperTests", dependencies: ["EmbraceWrapper"]),
+        .testTarget(name: "InstabugWrapperTests", dependencies: ["InstabugWrapper"])
     ]
 )
--- Tests/BugsnagWrapperTests/BugsnagWrapperTests.swift
+++ Tests/BugsnagWrapperTests/BugsnagWrapperTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import RumWrapper
 
-final class RumWrapperTests: XCTestCase {
+final class WrapperTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
